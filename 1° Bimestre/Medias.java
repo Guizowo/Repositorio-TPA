d6dca25997312fb3880d7f6ffcde720e62445169
Saturiy <86603410+Saturiy@users.noreply.github.com>
2024-09-24 19:54:14 -0300

    Add files via upload

---
+++ 1° Bimestre/Medias.java
@@ -0,0 +1,13 @@
+
+public class Medias {
+	public static void main(String args []) {
+		//Declaração de variáveis :D
+		double n1, n2, m;
+		n1 = 7;
+		n2 = 1;
+		m = (n1+n2)/2;
+		System.out.println("Sua média é "+ m);
+				
+		
+	}
+}

cda0bce0b68dce082bea3ed5718c95b23098baed
Guizo <86603410+Guizowo@users.noreply.github.com>
2024-09-25 20:14:58 -0300

    Update and rename 1° Bimestre/Medias.java to 1° Bimestre/Hello World /Medias.java

--- 1° Bimestre/Hello World /Medias.java
+++ 1° Bimestre/Hello World /Medias.java
@@ -1,12 +1,12 @@
 
 public class Medias {
 	public static void main(String args []) {
-		//DeclaraÁ„o de vari·veis :D
+		//Declara√ß√£o de vari√°veis :D
 		double n1, n2, m;
 		n1 = 7;
 		n2 = 1;
 		m = (n1+n2)/2;
-		System.out.println("Sua mÈdia È "+ m);
+		System.out.println("Sua m√©dia √© "+ m);
 				
 		
 	}

dc63b0a10956c79d959e812bd5fb9de0f652a656
Guizo <86603410+Guizowo@users.noreply.github.com>
2024-09-25 20:14:35 -0300

    Update and rename 1° Bimestre/AloMundo.java to 1° Bimestre/Hello World /AloMundo.java

--- 1° Bimestre/Hello World /AloMundo.java
+++ 1° Bimestre/Hello World /AloMundo.java
@@ -3,6 +3,6 @@ public class AloMundo {
 	public static void main(String args[]) {
 		
 		System.out.println("Hello World");
-		System.out.println("Alô Mundo");
+		System.out.println("AlÃ´ Mundo");
 	}
 }

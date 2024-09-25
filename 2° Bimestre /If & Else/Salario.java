e4ef90f99f2fe708435590fa24fb4096bdd7ac14
Guizo <86603410+Guizowo@users.noreply.github.com>
2024-09-25 20:25:41 -0300

    Update and rename Salario.java to Salario.java

--- 2° Bimestre /If & Else/Salario.java
+++ 2° Bimestre /If & Else/Salario.java
@@ -3,14 +3,14 @@ public class Salario {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		double sal;
-		System.out.println("Coloque o valor do seu sal·rio:");
+		System.out.println("Coloque o valor do seu sal√°rio:");
 		sal = ler.nextDouble();
 		
 		if (sal>1302) {
-			System.out.println("Seu sal·rio est· acima do sal·rio mÌnimo");
+			System.out.println("Seu sal√°rio est√° acima do sal√°rio m√≠nimo");
 
 		}else {
-			System.out.println("O seu sal·rio est· abaixo do sal·rio mÌnimo");
+			System.out.println("O seu sal√°rio est√° abaixo do sal√°rio m√≠nimo");
 		}
 		ler.close();
 	}
